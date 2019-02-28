c86cca1d8d6bc0e8a3e13c05514830ff31fd4e25
Tony <mourier.tony@outlook.com>
2019-02-28 15:16:52 +0100

    edit blade index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,22 +14,22 @@
 </content>
 <h1>Nos belles lunettes</h1>
 <div class="container">
-    <?php
-foreach ($produits as $produit) {
-    echo " <div class=\"card\" style=\"width:300px;\">
-            <p class=\"infos\">+ d'infos</p>
-            <a href=\"product?id=" . $produit->prod_id . "\"><img class=\"card-img-top\" src=\"../storage/img/produits/" . $produit->prod_img . "\"
-                    alt=\"image-card\" /></a>
-            <div class=\"card-body\">
-                <p class=\"card-text haut\">
-                    " . $produit->prod_marque . " - " . $produit->prod_nom . "
-                </p>
-                <p class=\"card-text\">Monture + verres à partir de</p>
-                <p class=\"prix\">" . $produit->prod_prix . ".00 €</p>
-                <p style=\"display:none;\">" . $produit->prod_id . "</p>
-            </div>
+
+    @foreach ($produits as $produit)
+    <div class="card" style="width:300px;">
+        <p class="infos">+ d'infos</p>
+        <a href="product?id={{$produit->prod_id}}"><img class="card-img-top"
+                src="../storage/img/produits/{{$produit->prod_img}}" alt="image-card" /></a>
+        <div class="card-body">
+            <p class="card-text haut">
+                {{$produit->prod_marque}} - {{$produit->prod_nom}}
+            </p>
+            <p class="card-text">Monture + verres à partir de</p>
+            <p class="prix"> {{$produit->prod_prix}}.00 €</p>
+            <p style="display:none;"> {{$produit->prod_id}} </p>
         </div>
-        ";}
-?>
+    </div>
+    @endforeach
+
 </div>
 @endsection
